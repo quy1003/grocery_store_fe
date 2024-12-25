46824f2f186782d9d42fc77be26572c425017ceb
lokhue <2151013052loc@gmail.com>
2024-12-25 13:39:10 +0700

    feat: base screen

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MyTabs from './src/components/MyTabs';
-import Welcome from './src/components/Login/Welcome';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MyTabs from "./src/components/MyTabs";
+import Welcome from "./src/components/Login/Welcome";
 
 export default function App() {
-  return (
-      <Welcome />
-  );
+  return <MyTabs />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
