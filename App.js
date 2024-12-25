9fdddb1614a8767440fc3c5fc8ad7d5ba9025881
quy1003 <quy021002@gmail.com>
2024-12-25 10:38:06 +0700

    mkdir styles

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MyTabs from './src/components/MyTabs';
+import Welcome from './src/components/Login/Welcome';
 
 export default function App() {
   return (
-      <MyTabs/>
+      <Welcome />
   );
 }
 
---
+++ src/components/Login/Welcome.js
@@ -0,0 +1,10 @@
+import { Text } from "react-native"
+
+
+const Welcome = () => {
+    return (
+        <Text>Welcome</Text>
+    )
+}
+
+export default Welcome
