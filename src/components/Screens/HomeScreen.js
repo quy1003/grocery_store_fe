59d80e1e398b9016ff3295169ff6d937dfde181f
lokhue <2151013052loc@gmail.com>
2024-12-27 16:27:58 +0700

    feat: Home screen base

--- src/components/Screens/HomeScreen.js
+++ src/components/Screens/HomeScreen.js
@@ -1,50 +1,79 @@
-import { Icon, SearchIcon } from "@/components/ui/icon";
-import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
 import BaseScreen from "@/src/components/BaseScreen";
-import MyInput from "@/src/components/MyInput";
-import ProductCard from "@/src/components/ProductCard";
-import Tabs from "@/src/components/Tabs";
+import MyInput from "@/src/components/ui/MyInput";
+import OfferCard from "@/src/components/Product/OfferCard";
+import ProductCard from "@/src/components/Product/ProductCard";
+import Tabs from "@/src/components/ui/Tabs";
 import React, { useState } from "react";
 import {
   View,
   Text,
   ScrollView,
   StyleSheet,
-  Touchable,
-  Button,
   TouchableOpacity,
 } from "react-native";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_PRODUCTS = gql`
+  {
+    products(search: "") {
+      items {
+        name
+        image {
+          url
+        }
+      }
+    }
+  }
+`;
 
 const HomeScreen = () => {
   const tabs = ["Fruits", "Vegetables", "Breads", "Others"];
 
   const [selectedTab, setSelectedTab] = useState(tabs[0]);
-  console.log(tabs);
-  tabs.map((m) => console.log(m));
+
+  // const { loading, error, data } = useQuery(GET_RECOMMENDED_VIDEOS);
+
+  // if (loading) return <Text>Loading...</Text>;
+  // if (error) return <Text>Error: {error.message}</Text>;
   return (
     <BaseScreen
       title="Welcome"
       subtitle="Find and order your fresh fruits & vegetables"
     >
-      <MyInput placeholder={"Search fresh fruits & vegetables..."} />
-      <Tabs tabs={tabs} />
-      <ScrollView horizontal>
-        <View style={styles.card}>
-          <ProductCard />
+      <View style={styles.container}>
+        <MyInput placeholder={"Search fresh fruits & vegetables..."} />
+        <Tabs tabs={tabs} activeTab={selectedTab} onPress={setSelectedTab} />
+        <ScrollView
+          horizontal
+          style={styles.productContainer}
+          showsHorizontalScrollIndicator={false}
+        >
+          <View style={styles.card}>
+            <ProductCard />
+          </View>
+          <View style={styles.card}>
+            <ProductCard />
+          </View>
+          <View style={styles.card}>
+            <ProductCard />
+          </View>
+          <View style={styles.card}>
+            <ProductCard />
+          </View>
+        </ScrollView>
+        <TouchableOpacity title="See All" style={styles.allBtn}>
+          <Text style={styles.textBtn}>See All</Text>
+        </TouchableOpacity>
+        <Text style={styles.textOffer}>Todays Offers</Text>
+        <View style={styles.offerContainer}>
+          <OfferCard />
+          <OfferCard />
+          <OfferCard />
+          <OfferCard />
+          <OfferCard />
+          <OfferCard />
         </View>
-        <View style={styles.card}>
-          <ProductCard />
-        </View>
-        <View style={styles.card}>
-          <ProductCard />
-        </View>
-        <View style={styles.card}>
-          <ProductCard />
-        </View>
-      </ScrollView>
-      <TouchableOpacity title="See All" style={styles.allBtn}>
-        <Text>See All</Text>
-      </TouchableOpacity>
+      </View>
     </BaseScreen>
   );
 };
@@ -53,20 +82,45 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
     flex: 1,
-    backgroundColor: "#fff",
+    // backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-between",
+    padding: 24,
   },
   card: {
-    marginRight: 10,
+    marginRight: 16,
   },
   allBtn: {
-    padding: 20,
+    padding: 8,
     borderColor: "#333",
     borderStyle: "solid",
     borderWidth: 1,
     borderRadius: 40,
-    width: 100,
+    width: 180,
+    marginVertical: 16,
+  },
+  textBtn: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  textOffer: {
+    alignSelf: "flex-start",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  productContainer: {
+    // marginTop: 10,
+    // height: "auto",
+  },
+  offerContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    paddingVertical: 10,
   },
 });
