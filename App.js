299c1cde94aea615c078e6bc74ea3eb6c2eefad7
pchihieuu <hieup3611@gmail.com>
2025-01-03 17:32:00 +0700

    change: signup screen

--- App.js
+++ App.js
@@ -23,8 +23,8 @@ export default function App() {
           <DemoComponent onFinish={handleFinish} />
         )}
       </View> */}
-      <MyTabs />
-      {/* <SignUpScreen /> */}
+      {/* <MyTabs /> */}
+      <SignUpScreen />
     </ApolloProvider>
   )
 }
