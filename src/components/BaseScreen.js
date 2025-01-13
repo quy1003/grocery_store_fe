f5a1dbfa0e471e4a6770de728cf9f7e9d88b7262
pchihieuu <hieup3611@gmail.com>
2025-01-14 01:07:12 +0700

    feat: update BaseScreen with rightComponent and separate styles into a dedicated file

--- src/components/BaseScreen.js
+++ src/components/BaseScreen.js
@@ -1,14 +1,14 @@
-import { useNavigation } from '@react-navigation/native'
-import React from 'react'
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
 import {
   View,
   Text,
   ScrollView,
-  StyleSheet,
   StatusBar,
   TouchableOpacity,
-} from 'react-native'
-import Icon from 'react-native-vector-icons/FontAwesome'
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import BaseStyles from "../styles/BaseStyles";
 
 const BaseScreen = ({
   children,
@@ -19,93 +19,48 @@ const BaseScreen = ({
   backPress = () => {},
   topRightPress = () => {},
   topRightPressText,
+  rightComponent,
 }) => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
   const handleBackPress = () => {
     if (backScreenName) {
-      navigation.navigate(backScreenName)
+      navigation.navigate(backScreenName);
     }
-  }
+  };
   return (
-    <View style={styles.container}>
+    <View style={BaseStyles.container}>
       <StatusBar backgroundColor="#56995d" barStyle="light-content" />
-      <ScrollView contentContainerStyle={styles.contentContainer}>
-        <View style={styles.header}>
-          <View style={styles.headerTop}>
+      <ScrollView contentContainerStyle={BaseStyles.contentContainer}>
+        <View style={BaseStyles.header}>
+          <View style={BaseStyles.headerTop}>
             <TouchableOpacity onPress={handleBackPress}>
               <Icon
                 name="chevron-left"
                 size={24}
-                fontWeight={'100'}
+                fontWeight={"100"}
                 color="#fff"
               />
             </TouchableOpacity>
-            <TouchableOpacity onPress={topRightPress}>
-              <Text>{topRightPressText}</Text>
-            </TouchableOpacity>
+            {rightComponent ? (
+              rightComponent
+            ) : (
+              <TouchableOpacity onPress={topRightPress}>
+                <Text>{topRightPressText}</Text>
+              </TouchableOpacity>
+            )}
           </View>
-          <Text style={styles.headerTitle}>{title}</Text>
-          <View style={styles.subtitleContainer}>
-            <Text style={styles.headerSubTitle}>{subtitle}</Text>
-            <Text style={styles.headerSubTitleLeft}>{leftSubtitle}</Text>
+          <Text style={BaseStyles.headerTitle}>{title}</Text>
+          <View style={BaseStyles.subtitleContainer}>
+            <Text style={BaseStyles.headerSubTitle}>{subtitle}</Text>
+            <Text style={BaseStyles.headerSubTitleLeft}>{leftSubtitle}</Text>
           </View>
         </View>
 
-        <View style={styles.content}>{children}</View>
+        <View style={BaseStyles.content}>{children}</View>
       </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default BaseScreen
-
-const styles = StyleSheet.create({
-  headerTop: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 8,
-  },
-  container: {
-    // flex: 1,
-    backgroundColor: '#f5f5f5',
-  },
-  contentContainer: {
-    flexGrow: 1,
-  },
-  header: {
-    backgroundColor: '#56995d',
-    padding: 24,
-    // paddingTop: 50,
-    paddingBottom: 80,
-    justifyContent: 'center',
-    height: 300,
-  },
-  headerTitle: {
-    color: '#fff',
-    fontSize: 28,
-    fontWeight: '600',
-  },
-  headerSubTitle: {
-    color: '#fff',
-    fontSize: 19,
-    width: '75%',
-  },
-  headerSubTitleLeft: {
-    color: '#fff',
-    fontSize: 22,
-  },
-  subtitleContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  content: {
-    flex: 1,
-    // padding: 24,
-    borderRadius: 20,
-    marginTop: -60,
-    backgroundColor: '#f5f5f5',
-    // height: 700,
-  },
-})
+export default BaseScreen;
